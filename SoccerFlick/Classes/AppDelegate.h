00bbc1f3939e6deb754f64e5c683310bd8d99300
pbajguz <pawelbajguz@Pawels-Mac.local>
2014-09-13 05:25:14 -0700

    Initial Commit

---
+++ SoccerFlick/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  SoccerFlick
+//
+//  Created by Pawel Bajguz on 13/09/2014.
+//  Copyright __MyCompanyName__ 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ SoccerFlick/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SoccerFlick
+//
+//  Created by Pawel Bajguz on 13/09/2014.
+//  Copyright __MyCompanyName__ 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
